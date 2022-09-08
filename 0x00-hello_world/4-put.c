47edab44d0ff533d3562f0d112df6b0e2d1f064f
zainab ali <zali48822@gmail.com>
2022-09-08 12:50:01 -0700

    puts

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-gcc put("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - print the string in the put function
+ *
+ *   *
+ *
+ *    * Return:0
+ *
+ *     */
+
+int main(void)
+
+{
+           puts("\"programming is like building a multilingual puzzel")
+	    return (0);
+
+}
