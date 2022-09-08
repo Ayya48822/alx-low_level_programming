3bae6af72defe9e11d6c5732496cfce456800cd1
zainab ali <zali48822@gmail.com>
2022-09-08 13:10:58 -0700

    puts

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -5,8 +5,7 @@
 /**
  *
  *  * main - print the string in the put function
- *Description:using main function
- *this program prints "Programming is like building a multilingual puzzle   
+ *   
  *    * Return:0
  */
 
