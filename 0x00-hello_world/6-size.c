1fe6b0075e8bdf688d5451a4cc6d0e39d813ec5c
zainab ali <zali48822@gmail.com>
2022-09-08 14:10:59 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+            char c;
+            int i;
+      long int li;
+  long long int lli;
+          float f;
+    printf("size of char is: %lu.\n", (unsigned long)sizeof(c));
+    printf("size of int is: %lu.\n", (unsigned long)sizeof(i));  
+    printf("size of long int: %lu.\n", (unsigned long)sizeof(li));
+    printf("size of long long int: %lu.\n" (unsigned long)sizeof(llu));
+    printf("size of float: %lu.\n" (unsigned long)sizeof(f));
+
+    return(0);
+}
