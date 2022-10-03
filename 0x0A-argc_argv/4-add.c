fa582366f9b75db9e46a00906ff392abded67e93
zainab ali <zali48822@gmail.com>
2022-10-03 07:14:59 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,15 @@
-#include <stdio.h>                                                                                         
+#include <stdio.h>
 #include <stdlib.h>
-/**                                                                                                        
+
+/**
 * main - adding two numbers
-* @argc: size of array                                                                                    
-* @argv: array passed                                                                                     
+* @argc: size of array
+* @argv: array to be passed
 *
-* Return: 0                                                                                               
+* Return: 0
 */
 
-int main(int argc, char *argv[])                                                                           
+int main(int argc, char *argv[])
 {
 	int i, sum;
 
@@ -18,15 +19,13 @@ int main(int argc, char *argv[])
 	{
 		if (i > 0)
 		{
-			if (atoi(argv[i]) == 0)
-			{		
-				return (printf("Error\n"), 1);
-			}
-			sum+=atoi(argv[i]);
-		}
-	}
-	printf("%d\n", sum);                                                                               
-	return (0);
+			if (atoi(argv[i]) == 0;
+					{
+					return (printf("Error\n"), 1);
+					}
+					sum += (atoi(argv[i]);
+				}
+				printf("%d\n", sum);
+				}
+				return (0);
 }
-
-					                                     
