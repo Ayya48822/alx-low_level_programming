3e8eb71262b65ebfc8f3de067fb06e1cf7df08fd
zainab ali <zali48822@gmail.com>
2022-10-02 22:34:20 -0700

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**                                                                                                        
 * main - print all arguments passes one after another
