908c6776261d79e05c1c7b05c0cc71d37ff39e03
zainab ali <zali48822@gmail.com>
2022-10-03 05:33:11 -0700

    names

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,11 @@
 * Return: Always 0
 */
 
-int main(int argc, char **argv)
+int main(int argc, char **argv[])
 {
 	while (argc--)
 	{	
-		printf("%s\n", *argv++);
+		printf("%s\n", *argv[0]);
 	}	
 	return (0);
 }
