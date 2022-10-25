825fa12c86d460da5619a925fb54e0c5ce454169
zainab ali <zali48822@gmail.com>
2022-10-25 16:53:36 -0700

    free list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,21 +8,41 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new node
+	list_t *current_node;
+	list_t *new_node;
 
-	if head == NULL
+	current_node = *head;
+	if (head == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(list_t));
-	if (new node == NULL)
+	new_node = current_node(str);
+	if (new_node == NULL)
 		return (NULL);
-	new node->str = strdup(str);
-	new node->len = strlen(str);
-	new node->next = NULL;
-	return (new node);
 
 	if (*head == NULL)
 	{
-		*head = new node;
-		return (new node);
+		*head = new_node;
+		return (*head);
 	}
+	while (current_node->next != NULL)
+		current_node = current_node->next;
+	current_node->next = new_node;
+	return (*head);
+}
+
+/**
+* create_node-create a new node
+* @str: string to add to the node
+* Return: a pointer to the allocated memory
+*/
+list_t *current_node(const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+	return (new_node);
 }
