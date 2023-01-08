37defc75486f8e04817e148fb666debc8e5818ce
zainab ali <zali48822@gmail.com>
2023-01-08 10:21:28 -0800

    index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,9 +5,10 @@
 * @key: key to generate index
 * @size:size of the array of the hash table
 *
-*Return: index of key/value pair stored in the array of the hash table
+* Return: index of key/value pair stored in the array of the hash table
 */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	return(hash_djb2(key) % size);
 }
